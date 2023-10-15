883b5baad1e1b897d81304ce5ac41ad16f74707d
Tony Meyer <tony.meyer@canonical.com>
2023-10-16 12:53:26 +1300

    Convert unittest unit tests to proposed API.

--- tests/unit/test_charm_unittest.py
+++ tests/unit/test_charm_unittest.py
@@ -5,7 +5,6 @@
 
 """Test the charm using Harness with unittest."""
 
-import os
 import time
 import unittest
 import unittest.mock
@@ -22,195 +21,130 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "simple"})
     def test_simple(self):
         """Verify that the 'simple' action runs without error."""
-        with unittest.mock.patch.object(self.harness.charm.framework.model._backend, "action_get"):
-            self.harness.charm.on.simple_action.emit()
+        out = self.harness.run_action("simple")
+        assert out.results is None
+        assert out.logs == []
+        assert not out.failure
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "input"})
     def test_input_default_value(self):
         """Verify that the 'input' action runs correctly (no arg is provided)."""
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            unittest.mock.MagicMock(
-                return_value=self.harness.charm.meta.actions["input"].parameters
-            ),
-        ):
-            with self.assertLogs(level="INFO") as cm:
-                self.harness.charm.on.input_action.emit()
+        with self.assertLogs(level="INFO") as cm:
+            out = self.harness.run_action("input")
+        assert out.results is None
+        assert out.logs == []
+        assert not out.failure
         default_value = self.harness.charm.meta.actions["input"].parameters["arg"]
         self.assertEqual(cm.output, [f"INFO:charm:The 'input' action says: {default_value}"])
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "input"})
     def test_input(self):
         """Verify that the 'input' action runs correctly (an arg is provided)."""
         response = "hello"
-        params = {"arg": response}
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            unittest.mock.MagicMock(return_value=params),
-        ):
-            with self.assertLogs(level="INFO") as cm:
-                self.harness.charm.on.input_action.emit()
+        with self.assertLogs(level="INFO") as cm:
+            out = self.harness.run_action("input", {"arg": response})
+        assert out.results is None
+        assert out.logs == []
+        assert not out.failure
         self.assertEqual(cm.output, [f"INFO:charm:The 'input' action says: {response}"])
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "multi-input"})
     def test_multi_input_default_value(self):
         """Verify that the 'multi-input' action runs correctly (no arg is provided)."""
-        params = {
-            key: details["default"]
-            for key, details in self.harness.charm.meta.actions["multi-input"].parameters.items()
-        }
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            unittest.mock.MagicMock(return_value=params),
-        ):
-            with self.assertLogs(level="INFO") as cm:
-                self.harness.charm.on.multi_input_action.emit()
+        with self.assertLogs(level="INFO") as cm:
+            out = self.harness.run_action("multi-input")
+        assert out.results is None
+        assert out.logs == []
+        assert not out.failure
+        defaults = self.harness.charm.meta.actions["input"].parameters
         self.assertEqual(
-            cm.output, [f"INFO:charm:The 'multi-input' action says: {params['arg1']}"]
+            cm.output, [f"INFO:charm:The 'multi-input' action says: {defaults['arg1']['default']}"]
         )
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "multi-input"})
     def test_multi_input_arg1(self):
         """Verify that the 'multi-input' action runs correctly (arg1 is provided)."""
-        params = {
-            key: details["default"]
-            for key, details in self.harness.charm.meta.actions["multi-input"].parameters.items()
-        }
         response = "hello"
-        params["arg1"] = response
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            unittest.mock.MagicMock(return_value=params),
-        ):
-            with self.assertLogs(level="INFO") as cm:
-                self.harness.charm.on.multi_input_action.emit()
+        with self.assertLogs(level="INFO") as cm:
+            out = self.harness.run_action("multi-input", params={"arg1": response})
+        assert out.results is None
+        assert out.logs == []
+        assert not out.failure
         self.assertEqual(cm.output, [f"INFO:charm:The 'multi-input' action says: {response}"])
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "multi-input"})
     def test_multi_input_arg2(self):
         """Verify that the 'multi-input' action runs correctly (arg2 is provided)."""
-        params = {
-            key: details["default"]
-            for key, details in self.harness.charm.meta.actions["multi-input"].parameters.items()
-        }
         count = 2
-        params["arg2"] = count
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            unittest.mock.MagicMock(return_value=params),
-        ):
-            with self.assertLogs(level="INFO") as cm:
-                self.harness.charm.on.multi_input_action.emit()
+        with self.assertLogs(level="INFO") as cm:
+            out = self.harness.run_action("multi-input", params={"arg2": count})
+        assert out.results is None
+        assert out.logs == []
+        assert not out.failure
+        defaults = self.harness.charm.meta.actions["input"].parameters
         self.assertEqual(
-            cm.output, [f"INFO:charm:The 'multi-input' action says: {params['arg1']}"] * count
+            cm.output,
+            [f"INFO:charm:The 'multi-input' action says: {defaults['arg1']['default']}"] * count,
         )
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "multi-input"})
     def test_multi_input_arg1_and_arg2(self):
         """Verify that the 'multi-input' action runs correctly (arg1 and arg2 are provided)."""
         response = "hello"
         count = 3
-        params = {"arg1": response, "arg2": count}
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            unittest.mock.MagicMock(return_value=params),
-        ):
-            with self.assertLogs(level="INFO") as cm:
-                self.harness.charm.on.multi_input_action.emit()
+        with self.assertLogs(level="INFO") as cm:
+            out = self.harness.run_action("multi-input", params={"arg1": response, "arg2": count})
+        assert out.results is None
+        assert out.logs == []
+        assert not out.failure
         self.assertEqual(
             cm.output, [f"INFO:charm:The 'multi-input' action says: {response}"] * count
         )
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "output"})
     def test_output(self):
         """Verify that the 'output' action runs correctly."""
         my_fortune = "favours the brave"
-        with unittest.mock.patch.object(self.harness.charm.framework.model._backend, "action_get"):
-            with unittest.mock.patch.object(
-                fortune, "get_random_fortune", return_value=my_fortune
-            ):
-                with unittest.mock.patch.object(
-                    self.harness.charm.framework.model._backend, "action_set"
-                ) as mock_set:
-                    self.harness.charm.on.output_action.emit()
-                    mock_set.assert_called_once_with({"fortune": my_fortune})
+        with unittest.mock.patch.object(fortune, "get_random_fortune", return_value=my_fortune):
+            out = self.harness.run_action("output")
+        assert out.results == {"fortune": my_fortune}
+        assert out.logs == []
+        assert not out.failure
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "logger"})
     def test_logger(self):
         """Verify that the 'simple' action runs without error."""
-        with unittest.mock.patch.object(self.harness.charm.framework.model._backend, "action_get"):
-            with unittest.mock.patch.object(
-                self.harness.charm.framework.model._backend, "action_log"
-            ) as mock_log:
-                # Also make this a bit faster :)
-                with unittest.mock.patch.object(time, "sleep"):
-                    self.harness.charm.on.logger_action.emit()
-                    mock_log.assert_has_calls(
-                        [
-                            unittest.mock.call("I'm counting to 10: 1"),
-                            unittest.mock.call("I'm counting to 10: 2"),
-                            unittest.mock.call("I'm counting to 10: 3"),
-                            unittest.mock.call("I'm counting to 10: 4"),
-                            unittest.mock.call("I'm counting to 10: 5"),
-                            unittest.mock.call("I'm counting to 10: 6"),
-                            unittest.mock.call("I'm counting to 10: 7"),
-                            unittest.mock.call("I'm counting to 10: 8"),
-                            unittest.mock.call("I'm counting to 10: 9"),
-                            unittest.mock.call("I'm counting to 10: 10"),
-                        ]
-                    )
+        # Also make this a bit faster :)
+        with unittest.mock.patch.object(time, "sleep"):
+            out = self.harness.run_action("logger")
+        assert out.results is None
+        assert out.logs == [
+            "I'm counting to 10: 1",
+            "I'm counting to 10: 2",
+            "I'm counting to 10: 3",
+            "I'm counting to 10: 4",
+            "I'm counting to 10: 5",
+            "I'm counting to 10: 6",
+            "I'm counting to 10: 7",
+            "I'm counting to 10: 8",
+            "I'm counting to 10: 9",
+            "I'm counting to 10: 10",
+        ]
+        assert not out.failure
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "bad"})
     def test_bad(self):
         """Verify that the 'bad' action runs without error (but fails)."""
-        with unittest.mock.patch.object(self.harness.charm.framework.model._backend, "action_get"):
-            with unittest.mock.patch.object(
-                self.harness.charm.framework.model._backend, "action_fail"
-            ) as mock_fail:
-                self.harness.charm.on.bad_action.emit()
-                mock_fail.assert_called_once()
+        out = self.harness.run_action("bad")
+        assert out.results is None
+        assert out.logs == []
+        assert out.failure == "Sorry, I just couldn't manage it."
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "combo"})
     def test_combo_fail(self):
         """Verify that the 'combo' action fails when instructed to do so."""
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            return_value={"should-fail": True},
-        ):
-            with unittest.mock.patch.object(
-                self.harness.charm.framework.model._backend, "action_fail"
-            ) as mock_fail:
-                self.harness.charm.on.combo_action.emit()
-                mock_fail.assert_called_once()
+        out = self.harness.run_action("combo", {"should-fail": True})
+        assert out.results is None
+        assert out.logs == []
+        assert out.failure
 
-    @unittest.mock.patch.dict(os.environ, {"JUJU_ACTION_NAME": "combo"})
     def test_combo(self):
         """Verify that the 'combo' action runs without error."""
         my_fortunes = ["magazine", "500", "cookie"]
-        with unittest.mock.patch.object(
-            self.harness.charm.framework.model._backend,
-            "action_get",
-            return_value={"should-fail": False},
-        ):
-            with unittest.mock.patch.object(
-                self.harness.charm.framework.model._backend, "action_log"
-            ) as mock_log:
-                with unittest.mock.patch.object(
-                    fortune, "get_random_fortune", side_effect=my_fortunes
-                ):
-                    with unittest.mock.patch.object(
-                        self.harness.charm.framework.model._backend, "action_set"
-                    ) as mock_set:
-                        self.harness.charm.on.combo_action.emit()
-                        mock_log.assert_has_calls([unittest.mock.call(f) for f in my_fortunes])
-                        mock_set.assert_called_once_with({"fortunes-told": 3})
+        with unittest.mock.patch.object(fortune, "get_random_fortune", side_effect=my_fortunes):
+            out = self.harness.run_action("combo")
+        assert out.results == {"fortunes-told": 3}
+        assert out.logs == my_fortunes
+        assert not out.failure
